d0d19ba81f9fc83ed91d2087349149202ccdf4c2
Monsingozzy <mohansinghyoyo88@gmail.com>
2022-06-03 15:18:46 +0530

    second

---
+++ src/newscomponent/News.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card, Button } from "react-bootstrap";
+
+export const News = () => {
+  //   const newsData = [];
+  const [State, Setstate] = useState([]);
+
+  useEffect(() => {
+    getdate();
+  }, []);
+
+  const getdate = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=0c83edc2d5414e10acea0a647f8f01de"
+      )
+      .then((response) => Setstate(response.data.articles))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Indian News App</h1>
+      </div>
+
+      {State.map((news) => (
+        <Card className="CradMain" style={{ width: "20rem" }}>
+          <Card.Img variant="top" className="imgsize" src={news.urlToImage} />
+          <Card.Body className="cardbody">
+            <Card.Title className="cardtitle">{news.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted font-weight-bold">
+              {news.description}
+            </Card.Subtitle>
+            {/* <Card.Text className="card-text">{news.content}</Card.Text> */}
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
